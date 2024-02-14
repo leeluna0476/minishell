cd506a066b23a3f4f2e381e05901fec2687cf2f5
Yuseung Kim <yusekim@c1r1s1.42seoul.kr>
2024-02-14 15:34:02 +0900

    working? I don't think so haha

--- Includes/execute.h
+++ Includes/execute.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seojilee <seojilee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yusekim <yusekim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 17:39:36 by yusekim           #+#    #+#             */
-/*   Updated: 2024/02/14 11:04:13 by seojilee         ###   ########.fr       */
+/*   Updated: 2024/02/14 12:51:10 by yusekim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 // execute.c
 void	do_execution(t_ast *tree, t_env_pack *pack, int level);
-void	build_cmd_pack(t_ast *tree, t_env_pack *pack, t_cmd *cmd);
+t_cmd	*build_cmd_pack(t_ast *tree, t_env_pack *pack);
 void	execute(t_ast *tree, t_env_pack *pack, int level);
 
 #endif
