a867da84287e156476925c82b41d18885e068a6e
Yuseung Kim <yusekim@c1r1s1.42seoul.kr>
2024-02-26 14:50:15 +0900

    FINISH WILDCARDDDDDDDDDDDDSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

--- srcs/expand/wildcard_utils.c
+++ srcs/expand/wildcard_utils.c
@@ -6,7 +6,7 @@
 /*   By: yusekim <yusekim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 10:47:48 by yusekim           #+#    #+#             */
-/*   Updated: 2024/02/26 11:22:53 by yusekim          ###   ########.fr       */
+/*   Updated: 2024/02/26 14:49:19 by yusekim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,11 @@ void	do_wildcard(t_c_expand *expand, int i)
 		{
 			temp_wildcard = expand_wildcard(expand->result[i]);
 			if (temp_wildcard)
+			{
 				expand->result = insert_strs(expand->result, temp_wildcard, &i);
+				split_free(temp_wildcard);
+				temp_wildcard = NULL;
+			}
 		}
 		i++;
 	}
@@ -44,28 +48,25 @@ int	scan_for_wildcard(char *str)
 
 char	**insert_strs(char **orig, char **new, int *i)
 {
-	int		o_idx;
-	int		n_idx;
+	char	**result;
 	int		orig_len;
 	int		new_len;
-	char	**res;
+	int		idx;
+	int		jdx;
 
 	orig_len = split_len(orig);
 	new_len = split_len(new);
-	res = ft_calloc(orig_len + new_len, sizeof(char *));
-	if (!res)
-		exit (1);
-	o_idx = -1;
-	while (++o_idx < *i)
-		res[o_idx] = orig[o_idx];
-	free(orig[o_idx]);
-	n_idx = -1;
-	while (++n_idx < new_len)
-		res[o_idx + n_idx] = new[n_idx];
-	while (++o_idx < orig_len)
-		res[o_idx + n_idx] = orig[o_idx];
-	free(orig);
-	free(new);
-	*i += new_len;
-	return (res);
+	result = malloc(sizeof(char *) * (orig_len + new_len));
+	result[orig_len + new_len -1] = NULL;
+	idx = -1;
+	while (++idx < *i)
+		result[idx] = ft_strdup(orig[idx]);
+	while (*new != NULL)
+		result[idx++] = ft_strdup(*new++);
+	jdx = 0;
+	while (idx < orig_len)
+		result[idx++] = ft_strdup(orig[*i + ++jdx]);
+	split_free(orig);
+	*i = *i + new_len - 1;
+	return (result);
 }
